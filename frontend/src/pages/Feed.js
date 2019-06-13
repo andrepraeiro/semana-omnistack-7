9881091ac021189e825623a5de563d754db92ed7
Andre Praeiro <andrepraeiro@gmail.com>
2019-06-13 17:47:12 -0400

    first commit

---
+++ frontend/src/pages/Feed.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+// import { Container } from './styles';
+
+export default class Feed extends Component {
+  render() {
+    return (
+    <h1>Feed</h1>
+    )
+  }
+}
