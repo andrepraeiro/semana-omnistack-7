52ddebe445cee64621cb0220d589d82a1ab4c90e
Andre Praeiro <andrepraeiro@gmail.com>
2019-06-13 16:50:04 -0400

    using sharp to resize images

--- backend/src/controllers/PostController.js
+++ backend/src/controllers/PostController.js
@@ -1,5 +1,8 @@
 const Post = require('../models/Post')
-//const sharp = require('sharp')
+const path = require('path')
+const sharp = require('sharp')
+const fs = require('fs')
+
 
 module.exports = {
     async index(req, res) {
@@ -10,13 +13,26 @@ module.exports = {
     async store(req, res) {
         const { author, place, description, hashtags } = req.body
         const { filename: image } = req.file
+
+        const [name] = image.split('.')
+        const fileName = `${name}.jpg`
+
+        await sharp(req.file.path)
+        .resize(500)
+        .jpeg({quality: 70})
+        .toFile(
+            path.resolve(req.file.destination, 'resized', fileName)
+        )
+
+        fs.unlinkSync(req.file.path)
+
         const post =
              await Post.create({
                 author,
                 place,
                 description,
                 hashtags,
-                image
+                image: fileName
             }).catch((r) => console.log(r))
         req.io.emit('post', post)
         return res.json(post)
