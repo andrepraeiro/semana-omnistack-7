4b69146cb1925d51249f444f77b867dbf62400d9
Andre Praeiro <andrepraeiro@gmail.com>
2019-06-17 11:13:23 -0400

    install styled-components

---
+++ frontend/src/pages/FeedStyles.js
@@ -0,0 +1,15 @@
+import styled from 'styled-components'
+
+export const PostList = styled.section `
+width: 100%;
+max-width: 580px;
+margin: 0 auto;
+padding: 0 30px;
+
+article {
+    background: #fff;
+    border: 1px solid #ddd;
+    margin-top: 30px;
+}
+`;
+
