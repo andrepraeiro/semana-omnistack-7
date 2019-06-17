54b34b01aba67cafbb44e6f9b0c22642034565a0
Andre Praeiro <andrepraeiro@gmail.com>
2019-06-17 06:09:16 -0400

    eslit fixes

--- backend/src/controllers/PostController.js
+++ backend/src/controllers/PostController.js
@@ -1,40 +1,41 @@
-const Post = require('../models/Post')
-const path = require('path')
-const sharp = require('sharp')
-const fs = require('fs')
+const path = require('path');
+const sharp = require('sharp');
+const fs = require('fs');
+const Post = require('../models/Post');
 
 
 module.exports = {
-    async index(req, res) {
-        const posts = await Post.find().sort('-createdAt')
-        res.json(posts)
-    },
+  async index(req, res) {
+    const posts = await Post.find().sort('-createdAt');
+    res.json(posts);
+  },
 
-    async store(req, res) {
-        const { author, place, description, hashtags } = req.body
-        const { filename: image } = req.file
+  async store(req, res) {
+    const {
+      author, place, description, hashtags,
+    } = req.body;
+    const { filename: image } = req.file;
 
-        const [name] = image.split('.')
-        const fileName = `${name}.jpg`
+    const [name] = image.split('.');
+    const fileName = `${name}.jpg`;
 
-        await sharp(req.file.path)
-        .resize(500)
-        .jpeg({quality: 70})
-        .toFile(
-            path.resolve(req.file.destination, 'resized', fileName)
-        )
+    await sharp(req.file.path)
+      .resize(500)
+      .jpeg({ quality: 70 })
+      .toFile(
+        path.resolve(req.file.destination, 'resized', fileName),
+      );
 
-        fs.unlinkSync(req.file.path)
+    fs.unlinkSync(req.file.path);
 
-        const post =
-             await Post.create({
-                author,
-                place,
-                description,
-                hashtags,
-                image: fileName
-            }).catch((r) => console.log(r))
-        req.io.emit('post', post)
-        return res.json(post)
-    }
-}
+    const post = await Post.create({
+      author,
+      place,
+      description,
+      hashtags,
+      image: fileName,
+    }).catch(r => console.log(r));
+    req.io.emit('post', post);
+    return res.json(post);
+  },
+};
--- backend/src/index.js
+++ backend/src/index.js
@@ -1,32 +1,32 @@
-const express = require('express')
-const mongoose = require('mongoose')
-const path = require('path')
-const cors = require('cors')
+const express = require('express');
+const mongoose = require('mongoose');
+const path = require('path');
+const cors = require('cors');
 
-const app = express()
+const app = express();
 
-const server = require('http').Server(app)
-const io = require('socket.io')(server)
-//mongoose.connect('mongodb+srv://semana:semana@cluster0-2w5cw.mongodb.net/test?retryWrites=true&w=majority',
-//{
+const server = require('http').Server(app);
+const io = require('socket.io')(server);
+// mongoose.connect('mongodb+srv://semana:semana@cluster0-2w5cw.mongodb.net/test?retryWrites=true&w=majority',
+// {
 //    useNewUrlParser:true,
-//})
+// })
 
 
 mongoose.connect('mongodb://127.0.0.1:27017/semana',
-{
-    useNewUrlParser:true,
-})
+  {
+    useNewUrlParser: true,
+  });
 
 app.use((req, res, next) => {
-    req.io = io
-    next()
-})
+  req.io = io;
+  next();
+});
 
-app.use(cors())
+app.use(cors());
 
-app.use('/files', express.static(path.resolve(__dirname,'..', 'uploads', 'resized')))
+app.use('/files', express.static(path.resolve(__dirname, '..', 'uploads', 'resized')));
 
-app.use(require('./routes'))
+app.use(require('./routes'));
 
-server.listen(3333)
+server.listen(3333);
